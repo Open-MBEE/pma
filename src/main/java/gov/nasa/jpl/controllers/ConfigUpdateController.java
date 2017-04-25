5fd9cba29e75416069e1f1893fed619b97c1764f
Tommy Hang <tommy.hang@jpl.nasa.gov>
2017-04-25 08:11:16 -0700

    Adding example code to read files template files.

---
+++ src/main/java/gov/nasa/jpl/controllers/ConfigUpdateController.java
@@ -0,0 +1,92 @@
+package gov.nasa.jpl.controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+/**
+ * Endpoint for updating the properties file.
+ */
+import java.sql.Timestamp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import gov.nasa.jpl.jenkinsUtil.JenkinsBuildConfig;
+import gov.nasa.jpl.jenkinsUtil.JenkinsEngine;
+import gov.nasa.jpl.mmsUtil.MMSUtil;
+import gov.nasa.jpl.model.JobFromVE;
+import gov.nasa.jpl.model.JobInstanceFromVE;
+
+@Controller
+public class ConfigUpdateController {
+
+	/**
+	 * Returns all the jobs of a project.
+	 * @param projectID
+	 * @param refID
+	 * @return
+	 */
+	@RequestMapping(value = "/admin/", method = RequestMethod.GET)
+	@ResponseBody
+	public String getJobs(@PathVariable String projectID, @PathVariable String refID,@RequestParam String alf_ticket,@RequestParam String mmsServer) {
+		
+		MMSUtil mmsUtil = new MMSUtil(alf_ticket);
+		mmsUtil.getJobElements(mmsServer,projectID, refID);
+		
+		return "job" + "\n" + projectID + "\n" + refID+ "\n"+alf_ticket;
+	}
+	
+	@RestController
+	@EnableAutoConfiguration
+	public class ResourcesController {
+	    @Autowired
+	    private ResourceLoader resourceLoader;
+
+	    @RequestMapping(value = "/file", method = RequestMethod.GET)
+	    public String getResources() throws IOException {
+	        InputStream is = resourceLoader.getResource("classpath:templates/pmaTestJenkins2.sh").getInputStream();
+	        BufferedReader br = null;
+			StringBuilder sb = new StringBuilder();
+
+			String line;
+			try {
+
+				br = new BufferedReader(new InputStreamReader(is));
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+					sb.append("\n");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+	        return "the content of resources:" + sb.toString();
+//			return new String(Files.readAllBytes(Paths.get("src/main/resources/templates/pmaTestJenkins2.sh")));
+	    }
+
+	}
+
+}
