ce5c202b80242f26ea38c7da248818ff6e6a9db7
Tommy Hang <tommy.hang@jpl.nasa.gov>
2017-05-01 14:14:51 -0700

    updating jenkins template and properties file

--- src/main/java/gov/nasa/jpl/controllers/ConfigUpdateController.java
+++ src/main/java/gov/nasa/jpl/controllers/ConfigUpdateController.java
@@ -6,9 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
