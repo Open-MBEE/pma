3d9c8be79ff7621d2d9a8de39cc07422c40e2731
Tommy Hang <tommy.hang@jpl.nasa.gov>
2017-06-02 09:54:25 -0700

    commented out magicdraw run in docgen template for now.

--- src/main/resources/templates/docgen2.sh
+++ src/main/resources/templates/docgen2.sh
@@ -1,4 +1,4 @@
-# temporary docgen job
+# docgen job
 set +x +e # x is for quieter logs. e is so the job can continue after a failure.
 
 export MAGICDRAW_HOME="/opt/local/MD"
@@ -53,30 +53,29 @@ echo --mmsHost $MMS_HOST --mmsPort $MMS_PORT --mmsUsername $MMS_USERNAME --mmsPa
  --twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME --twcPassword $TWC_PASSWORD \
  --projectId $PROJECT_ID --refId $REF_ID --targetViewId $TARGET_VIEW_ID --jobId $JOB_ID
 
-# DISPLAY=:9001
-# vncserver -kill "$DISPLAY" 2> /dev/null || true
-# vncserver "$DISPLAY" -SecurityTypes None
-# export DISPLAY
-#
-# bash /opt/local/MD/analysis/automatedviewgeneration.sh \
-# --mmsHost $MMS_HOST --mmsPort $MMS_PORT --mmsUsername $MMS_USERNAME --mmsPassword $MMS_PASSWORD \
-# --twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME \
-# --twcPassword $TWC_PASSWORD --projectId $PROJECT_ID --refId $REF_ID \
-# --targetViewId $TARGET_VIEW_ID --generateRecursively
-#
-# mdExitCode=$?
-# echo MDEXITCODE $mdExitCode
-#
-# vncserver -kill "$DISPLAY"
-#
-# if (($mdExitCode == 0))
-# then
-# 	status=Completed
-# else
-# 	status=Failed
-# fi
+DISPLAY=:9001
+vncserver -kill "$DISPLAY" 2> /dev/null || true
+vncserver "$DISPLAY" -SecurityTypes None
+export DISPLAY
 
-status=Completed
+bash /opt/local/MD/analysis/automatedviewgeneration.sh \
+--mmsHost $MMS_HOST --mmsPort $MMS_PORT --mmsUsername $MMS_USERNAME --mmsPassword $MMS_PASSWORD \
+--twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME \
+--twcPassword $TWC_PASSWORD --projectId $PROJECT_ID --refId $REF_ID \
+--targetViewId $TARGET_VIEW_ID --generateRecursively
+
+mdExitCode=$?
+echo MDEXITCODE $mdExitCode
+
+vncserver -kill "$DISPLAY"
+
+if (($mdExitCode == 0))
+then
+	status=Completed
+else
+	status=Failed
+fi
+
 echo status $status
 
 ticket=$(curl -X POST -H Content-Type:application/json --data "{"username":$MMS_USERNAME, "password":$MMS_PASSWORD}" https://${MMS_HOST}/alfresco/service/api/login)
@@ -101,4 +100,4 @@ pmaUpdateJSON="{$ticketKey:$ticket,$valueKey:$param}" #JSON to send to PMA
 pmaResponse=$(curl -X POST -H Content-Type:application/json --data "$pmaUpdateJSON" $PMA_HOST/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
 echo pmaResponse $pmaResponse
 
-#exit $mdExitCode
+exit $mdExitCode
