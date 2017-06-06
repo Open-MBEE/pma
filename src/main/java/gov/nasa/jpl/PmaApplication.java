ab769b8f75be63c867e236c4c829d32532486980
Tommy Hang <tommy.hang@jpl.nasa.gov>
2017-06-06 13:06:09 -0700

    adding null check for ssl key password

--- src/main/java/gov/nasa/jpl/PmaApplication.java
+++ src/main/java/gov/nasa/jpl/PmaApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PmaApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(PmaApplication.class, args);
 		SpringApplication application = new SpringApplication(PmaApplication.class);
 		Map<String, Object> map = new HashMap<>();
 		
@@ -23,13 +22,16 @@ public class PmaApplication {
 		{
 			String sslKeyPassword = System.getenv("ssl_key_password");
 			String sslKeyAlias = System.getenv("ssl_key_alias");
-			
-			map.put("server.ssl.key-store", sslCertificatePath);
-			map.put("server.ssl.key-store-password", sslKeyPassword);
-			
-			map.put("server.ssl.keyAlias", sslKeyAlias);	
+			if((sslKeyAlias!=null)&&(sslKeyPassword!=null))
+			{
+				System.out.println("sslkeypass: " + sslKeyPassword);
+				System.out.println("sslkeyalias: " + sslKeyAlias);
+				map.put("server.ssl.key-store", sslCertificatePath);
+				map.put("server.ssl.key-store-password", sslKeyPassword);
+				map.put("server.ssl.keyAlias", sslKeyAlias);
+				System.out.println("STARTING IN SSL");
+			}
 		}
-		
 		map.put("SERVER_PORT", "8443");
 		application.setDefaultProperties(map);
 		application.run(args);
