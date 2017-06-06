76fbbdfec3b562dd6508c69dfb782151733b7a34
Tommy Hang <tommy.hang@jpl.nasa.gov>
2017-06-06 14:37:49 -0700

    Updating docgen shell script.

--- src/main/resources/templates/docgen.sh
+++ src/main/resources/templates/docgen.sh
@@ -1,4 +1,4 @@
-# temporary docgen job
+# docgen job
 set +x +e # x is for quieter logs. e is so the job can continue after a failure.
 
 export MAGICDRAW_HOME="/opt/local/MD"
@@ -45,30 +45,31 @@ param='"'$status'"'
 pmaUpdateJSON="{$ticketKey:$ticket,$valueKey:$param}" #JSON to send to PMA
 
 
-pmaResponse=$(curl -X POST -H Content-Type:application/json --data $pmaUpdateJSON $PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
+pmaResponse=$(curl -X POST -H Content-Type:application/json --data $pmaUpdateJSON https://$PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
 echo pmaResponse $pmaResponse
 
 
 echo --mmsHost $MMS_HOST --mmsPort $MMS_PORT --mmsUsername $MMS_USERNAME --mmsPassword $MMS_PASSWORD \
- --twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME --twcPassword $TWC_PASSWORD \
- --projectId $PROJECT_ID --refId $REF_ID --targetViewId $TARGET_VIEW_ID --jobId $JOB_ID
+--twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME \
+--twcPassword $TWC_PASSWORD --projectId $PROJECT_ID --refId $REF_ID \
+--targetViewId $TARGET_VIEW_ID --pmaHost $PMA_HOST --pmaPort $PMA_PORT --pmaJobId $JOB_ID --generateRecursively
 
-# DISPLAY=:9001
-# vncserver -kill "$DISPLAY" 2> /dev/null || true
-# vncserver "$DISPLAY" -SecurityTypes None
-# export DISPLAY
-#
+DISPLAY=:9001
+vncserver -kill "$DISPLAY" 2> /dev/null || true
+vncserver "$DISPLAY" -SecurityTypes None
+export DISPLAY
+
 bash /opt/local/MD/analysis/automatedviewgeneration.sh \
 --mmsHost $MMS_HOST --mmsPort $MMS_PORT --mmsUsername $MMS_USERNAME --mmsPassword $MMS_PASSWORD \
 --twcHost $TWC_HOST --twcPort $TWC_PORT --twcUsername $TWC_USERNAME \
 --twcPassword $TWC_PASSWORD --projectId $PROJECT_ID --refId $REF_ID \
---targetViewId $TARGET_VIEW_ID --generateRecursively
-#
-# mdExitCode=$?
-# echo MDEXITCODE $mdExitCode
-#
-# vncserver -kill "$DISPLAY"
-#
+--targetViewId $TARGET_VIEW_ID --pmaHost $PMA_HOST --pmaPort $PMA_PORT --pmaJobId $JOB_ID --generateRecursively
+
+mdExitCode=$?
+echo MDEXITCODE $mdExitCode
+
+vncserver -kill "$DISPLAY"
+
 if (($mdExitCode == 0))
 then
 	status=Completed
@@ -76,7 +77,6 @@ else
 	status=Failed
 fi
 
-status=Completed
 echo status $status
 
 ticket=$(curl -X POST -H Content-Type:application/json --data "{"username":$MMS_USERNAME, "password":$MMS_PASSWORD}" https://${MMS_HOST}/alfresco/service/api/login)
@@ -90,15 +90,15 @@ param='"'$status'"'
 
 pmaUpdateJSON="{$ticketKey:$ticket,$valueKey:$param}" #JSON to send to PMA
 
-pmaResponse=$(curl -X POST -H Content-Type:application/json --data "$pmaUpdateJSON" $PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
+pmaResponse=$(curl -X POST -H Content-Type:application/json --data "$pmaUpdateJSON" https://$PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
 echo pmaResponse $pmaResponse
 
-# artifactLink=$BUILD_URL"artifact/MDNotificationWindowText.html"
+artifactLink=$BUILD_URL"artifact/MDNotificationWindowText.html"
 
-# param='"'$artifactLink'"'
+param='"'$artifactLink'"'
 
-# pmaUpdateJSON="{$ticketKey:$ticket,$valueKey:$param}" #JSON to send to PMA
-# pmaResponse=$(curl -X POST -H Content-Type:application/json --data "$pmaUpdateJSON" $PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
-# echo pmaResponse $pmaResponse
+pmaUpdateJSON="{$ticketKey:$ticket,$valueKey:$param}" #JSON to send to PMA
+pmaResponse=$(curl -X POST -H Content-Type:application/json --data "$pmaUpdateJSON" https://$PMA_HOST:$PMA_PORT/projects/$PROJECT_ID/refs/master/jobs/$JOB_BASE_NAME/instances/$BUILD_NUMBER/jobStatus?mmsServer=${MMS_HOST})
+echo pmaResponse $pmaResponse
 
-#exit $mdExitCode
+exit $mdExitCode
